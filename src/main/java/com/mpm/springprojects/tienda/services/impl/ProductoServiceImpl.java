0fbc57e9d2793abfa1a092782f5f0820a28d46b8
Marta Perez <mpermor2205@g.educaand.es>
2022-11-24 12:09:28 +0100

    implementado ProductoServiceImpl

---
+++ src/main/java/com/mpm/springprojects/tienda/services/impl/ProductoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.mpm.springprojects.tienda.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mpm.springprojects.tienda.dao.ProductosDAO;
+import com.mpm.springprojects.tienda.model.Producto;
+import com.mpm.springprojects.tienda.services.ProductosService;
+
+@Service
+public class ProductoServiceImpl implements ProductosService{
+
+    @Autowired
+    ProductosDAO productosDAO;
+
+    @Override
+    public List<Producto> findAll() {
+        return productosDAO.findAll();
+    }
+
+    @Override
+    public Producto findById(int codigo) {
+        return productosDAO.findById(codigo);
+    }
+
+    @Override
+    public void insert(Producto Producto) {
+        productosDAO.insert(Producto);
+    }
+
+    @Override
+    public void update(Producto Producto) {
+        productosDAO.update(Producto);
+    }
+
+    @Override
+    public void delete(int codigo) {
+        productosDAO.delete(codigo);
+    }
+    
+    
+}
