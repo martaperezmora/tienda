67570fc9f1842d41b17dd44f1725c72206a76124
marta perez <mpermor2205@g.educaand.es>
2022-12-02 15:37:56 +0100

    editado clientesServices

--- src/main/java/com/mpm/springprojects/tienda/services/ClientesService.java
+++ src/main/java/com/mpm/springprojects/tienda/services/ClientesService.java
@@ -2,13 +2,14 @@ package com.mpm.springprojects.tienda.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import com.mpm.springprojects.tienda.model.Cliente;
 
 public interface ClientesService {
-    
-    public List<Cliente> findAll();
+    public Page<Cliente> findAll(Pageable page);
     public Cliente findById(int codigo);
-    public void insert(Cliente cliente);
-    public void update(Cliente cliente);
+    public void insert(Cliente Producto);
+    public void update(Cliente Producto);
     public void delete(int codigo);
 }
