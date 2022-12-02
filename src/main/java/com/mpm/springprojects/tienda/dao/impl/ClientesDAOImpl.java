8e2f9bcd968d5304438745a7c75c5955fe924bf8
marta perez <mpermor2205@g.educaand.es>
2022-12-02 16:20:59 +0100

    editado ClientesDAOImpl

--- src/main/java/com/mpm/springprojects/tienda/dao/impl/ClientesDAOImpl.java
+++ src/main/java/com/mpm/springprojects/tienda/dao/impl/ClientesDAOImpl.java
@@ -1,7 +1,5 @@
 package com.mpm.springprojects.tienda.dao.impl;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
