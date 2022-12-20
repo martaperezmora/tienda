072bbdde9d685ed22f25b428f1f730efbcaf6b55
Marta Perez <mpermor2205@g.educaand.es>
2022-12-20 10:44:57 +0100

    creada clase pedido

---
+++ src/main/java/com/mpm/springprojects/tienda/model/Pedido.java
@@ -0,0 +1,78 @@
+package com.mpm.springprojects.tienda.model;
+
+import java.util.List;
+
+public class Pedido {
+    private int codigo;
+    private Cliente cliente;
+    private List<Producto> productos;
+    private String fecha;
+    private double importeTotal;
+
+    
+    public Pedido(int codigo, Cliente cliente, List<Producto> productos, String fecha, double importeTotal) {
+        this.codigo = codigo;
+        this.cliente = cliente;
+        this.productos = productos;
+        this.fecha = fecha;
+        this.importeTotal = importeTotal;
+    }
+    public Pedido(int codigo) {
+        this.codigo = codigo;
+    }
+    public Pedido() {
+    }
+
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    public List<Producto> getProductos() {
+        return productos;
+    }
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public double getImporteTotal() {
+        return importeTotal;
+    }
+    public void setImporteTotal(double importeTotal) {
+        this.importeTotal = importeTotal;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigo;
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pedido other = (Pedido) obj;
+        if (codigo != other.codigo)
+            return false;
+        return true;
+    }
+}
