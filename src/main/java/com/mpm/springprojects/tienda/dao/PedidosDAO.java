990dd4e400b2ce1609213e1d0edf34420be77dbc
Marta Perez <mpermor2205@g.educaand.es>
2022-12-20 12:00:56 +0100

    añadido findByCliente en PedidosDAO

---
+++ src/main/java/com/mpm/springprojects/tienda/dao/PedidosDAO.java
@@ -0,0 +1,15 @@
+package com.mpm.springprojects.tienda.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.mpm.springprojects.tienda.model.Pedido;
+
+public interface PedidosDAO {
+    public Page<Pedido> findAll(Pageable page);
+    public Pedido findById(int codigo);
+    public Page<Pedido> findByCliente(int codigo, Pageable page);
+    public void insert(Pedido Pedido);
+    public void update(Pedido Pedido);
+    public void delete(int codigo);
+}
