58b3dc771003e0989f45d9b9b57b3a1540aabcc6
Marta Perez <mpermor2205@g.educaand.es>
2023-01-18 12:40:31 +0100

    creado repositorio cliente y producto

--- create.sql
+++ create.sql
@@ -1,3 +1,15 @@
 create table hibernate_sequence (next_val bigint) engine=InnoDB;
 insert into hibernate_sequence values ( 1 );
 create table proveedor (codigo integer not null, apellidos varchar(255), nombre varchar(255), primary key (codigo)) engine=InnoDB;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table proveedor (codigo integer not null, apellidos varchar(255), nombre varchar(255), primary key (codigo)) engine=InnoDB;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table proveedor (codigo integer not null, apellidos varchar(255), nombre varchar(255), primary key (codigo)) engine=InnoDB;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table proveedor (codigo integer not null, apellidos varchar(255), nombre varchar(255), primary key (codigo)) engine=InnoDB;
+create table hibernate_sequence (next_val bigint) engine=InnoDB;
+insert into hibernate_sequence values ( 1 );
+create table proveedor (codigo integer not null, apellidos varchar(255), nombre varchar(255), primary key (codigo)) engine=InnoDB;
