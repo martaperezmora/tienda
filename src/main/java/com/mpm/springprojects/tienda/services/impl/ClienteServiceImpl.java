1c937cbeef32388022e654c412b9a23c86f18772
Marta Perez <mpermor2205@g.educaand.es>
2023-01-19 10:36:20 +0100

    editado servicio cliente; implementado repository

--- src/main/java/com/mpm/springprojects/tienda/services/impl/ClienteServiceImpl.java
+++ src/main/java/com/mpm/springprojects/tienda/services/impl/ClienteServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mpm.springprojects.tienda.services.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,10 +9,52 @@ import org.springframework.stereotype.Service;
 
 import com.mpm.springprojects.tienda.dao.ClientesDAO;
 import com.mpm.springprojects.tienda.model.Cliente;
+import com.mpm.springprojects.tienda.repository.ClienteRepository;
 import com.mpm.springprojects.tienda.services.ClientesService;
 
+// código para usar el repository
 @Service
 public class ClienteServiceImpl implements ClientesService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Override
+    public Page<Cliente> findAll(Pageable page) {
+        return clienteRepository.findAll(page);
+    }
+
+    @Override
+    public Cliente findById(int codigo) {
+        Optional<Cliente> findById = clienteRepository.findById(codigo);
+        if(findById != null){
+            return findById.get();
+        }
+        return null;
+    }
+
+    @Override
+    public void insert(Cliente cliente) {
+        clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void update(Cliente cliente) {
+        clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void delete(int codigo) {
+        clienteRepository.deleteById(codigo);
+    }
+
+}
+
+/*
+ * código para utilizar el DAO
+ 
+ @Service
+ public class ClienteServiceImpl implements ClientesService {
 
     @Autowired
     ClientesDAO clientesDAO;
@@ -39,5 +83,6 @@ public class ClienteServiceImpl implements ClientesService {
     public void delete(int codigo) {
         clientesDAO.delete(codigo);
     }
-
 }
+
+*/
