6ffb312b8d17a8a6af635ba0fabcb138459a9123
Marta Perez <mpermor2205@g.educaand.es>
2023-01-19 11:13:54 +0100

    implementado ProductoServiceImpl con repository

--- src/main/java/com/mpm/springprojects/tienda/services/impl/ProductoServiceImpl.java
+++ src/main/java/com/mpm/springprojects/tienda/services/impl/ProductoServiceImpl.java
@@ -1,5 +1,7 @@
 package com.mpm.springprojects.tienda.services.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,7 +9,55 @@ import org.springframework.stereotype.Service;
 
 import com.mpm.springprojects.tienda.dao.ProductosDAO;
 import com.mpm.springprojects.tienda.model.Producto;
+import com.mpm.springprojects.tienda.repository.ProductoRepository;
 import com.mpm.springprojects.tienda.services.ProductosService;
+
+@Service
+public class ProductoServiceImpl implements ProductosService {
+
+    @Autowired
+    ProductoRepository productoRepository;
+
+    @Override
+    public Page<Producto> findAll(Pageable page) {
+        return productoRepository.findAll(page);
+    }
+
+    @Override
+    public Producto findById(int codigo) {
+        Optional<Producto> findById = productoRepository.findById(codigo);
+        if(findById != null){
+            return findById.get();
+        }
+        return null;
+    }
+
+    @Override
+    public void insert(Producto Producto) {
+        productoRepository.save(Producto);
+    }
+
+    @Override
+    public void update(Producto producto) {
+        productoRepository.save(producto);
+
+        /*
+        if (producto.getImagen() != null && producto.getImagen().length > 0) {
+            productoRepository.updateImage(producto);
+        }
+        */
+    }
+
+    @Override
+    public void delete(int codigo) {
+        productoRepository.deleteById(codigo);
+    }
+
+}
+
+
+/* 
+codigo para usar el dao
 
 @Service
 public class ProductoServiceImpl implements ProductosService {
@@ -45,3 +95,4 @@ public class ProductoServiceImpl implements ProductosService {
     }
 
 }
+*/
